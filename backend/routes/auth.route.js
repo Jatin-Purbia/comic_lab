16551cc3a5f092646d1a7d8ea5ecda96f6b09056
AadityaSharma1001 <b23me1001@iitj.ac.in>
2025-04-13 01:53:20 +0530

    added login route

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,11 +1,11 @@
 import express from 'express';
-import { googleLogin, signUp} from '../controllers/auth.controller.js';
+import { googleLogin, signUp, login} from '../controllers/auth.controller.js';
 
 
 const router = express.Router();
 
 router.post('/google-login', googleLogin);
 router.post('/signup', signUp);
-// router.post('/login', login);
+router.post('/login', login);
 
 export default router;
